eaf864225133ddef472a042c85e2a09879bbdb0d
CarsonJoe <carsongragg@gmail.com>
2025-01-25 00:00:21 -0800

    cleaned up

--- save js/constants.js
+++
@@ -1,6 +0,0 @@
-// constants.js
-export const CHUNK_SIZE = 4;
-export const CHUNK_HEIGHT = 72;
-export const WATER_LEVEL = 24;
-export const BEACH_LEVEL = WATER_LEVEL + 2;
-export const RENDER_DISTANCE = 20;
